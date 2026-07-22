87b448b22d58eb48c69c70eb8c01a612e551383e
rohithh55 <rohiithh5@gmail.com>
2026-07-22 13:01:42 +0530

    Format S3 module

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -28,18 +28,22 @@ resource "aws_s3_bucket_versioning" "versioning" {
 resource "aws_kms_key" "s3" {
   description             = "S3 KMS Key"
   deletion_window_in_days = 7
-}
+  enable_key_rotation     = true
 
-resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
-
-  bucket = aws_s3_bucket.bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      kms_master_key_id = aws_kms_key.s3.arn
-      sse_algorithm     = "aws:kms"
-    }
-  }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "EnableRootPermissions"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::522814728916:root"
+        }
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 #############################
